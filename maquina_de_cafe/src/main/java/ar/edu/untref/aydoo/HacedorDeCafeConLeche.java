5e8f905fa71a78d2b936c7877c0ba44c0d4ae956
Matías Mansilla <tute.mdm@gmail.com>
2016-04-05 02:37:17 -0300

    Se arreglan los problemas en maquina_de_cafe

--- maquina_de_cafe/src/main/java/ar/edu/untref/aydoo/HacedorDeCafeConLeche.java
+++ maquina_de_cafe/src/main/java/ar/edu/untref/aydoo/HacedorDeCafeConLeche.java
@@ -5,6 +5,11 @@ public class HacedorDeCafeConLeche {
 	private Cafetero cafetero;
 	private Lechero lechero;
 	
+	public HacedorDeCafeConLeche(){
+		cafetero = new Cafetero();
+		lechero = new Lechero();
+	}
+	
 	public Vaso prepararEnEsteVaso(Vaso vaso){
 		this.cafetero.prepararEnEsteVaso(vaso);
 		this.lechero.prepararEnEsteVaso(vaso);
--- maquina_de_cafe/src/main/java/ar/edu/untref/aydoo/HacedorDeTeConLeche.java
+++ maquina_de_cafe/src/main/java/ar/edu/untref/aydoo/HacedorDeTeConLeche.java
@@ -5,6 +5,11 @@ public class HacedorDeTeConLeche {
 	HacedorDeTe hacedorDeTe;
 	Lechero lechero;
 
+	public HacedorDeTeConLeche(){
+		hacedorDeTe = new HacedorDeTe();
+		lechero = new Lechero();
+	}
+	
 	public Vaso prepararEnEsteVaso(Vaso vaso) {
 		 this.hacedorDeTe.prepararEnEsteVaso(vaso);
 		 this.lechero.prepararEnEsteVaso(vaso);
