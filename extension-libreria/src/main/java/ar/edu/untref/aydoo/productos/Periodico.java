ae885ce66a3909d4cbc946ead85899dc2823095c
Matías Mansilla <tute.mdm@gmail.com>
2016-04-25 20:16:58 -0300

    Refactor para que el Diario se ajuste a la consigna y se llame Periodico

---
+++ extension-libreria/src/main/java/ar/edu/untref/aydoo/productos/Periodico.java
@@ -0,0 +1,9 @@
+package ar.edu.untref.aydoo.productos;
+
+public class Periodico extends ProductoSuscribible {
+
+	public Periodico(String nombre, int periodicidad, double precio) {
+		super(nombre, periodicidad, precio);
+	}
+	
+}
